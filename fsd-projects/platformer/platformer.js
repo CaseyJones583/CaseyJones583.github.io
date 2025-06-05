e9db8d7459e5f60da1934ca4735c0c2bd43649b9
CaseyJones583 <blo0persco0per28@gmail.com>
2025-06-05 17:00:17 +0000

    started platformer

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,11 +27,13 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
+createPlatform(400, 200, 20, 700);
+createPlatform(300, 650, 100, 20);
+createPlatform(300, 650, 100, 20);
 
 
 
