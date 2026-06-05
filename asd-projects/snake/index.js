8d97f95c5c4e70fdb6f9bf8a068f44883ab7e9f2
CaseyJones583 <blo0persco0per28@gmail.com>
2026-06-05 15:42:37 +0000

    completed todo 8

--- asd-projects/snake/index.js
+++ asd-projects/snake/index.js
@@ -138,9 +138,19 @@ function moveSnake() {
     of snake.head.direction which may be one of "left", "right", "up", or "down"
   */
 
-
-
-
+if (snake.head.direction === "left") {
+  snake.head.column = snake.head.column - 1;
+}
+else if (snake.head.direction === "right") {
+  snake.head.column = snake.head.column + 1;
+}
+else if (snake.head.direction === "up") {
+  snake.head.row = snake.head.row - 1;
+}
+else if (snake.head.direction === "down") {
+  snake.head.row = snake.head.row + 1;
+}
+repositionSquare(snake.head)
 }
 
 // TODO 9: Create a new helper function
